c75ddb3e1fd539de04f81a3325fb9eeff0c786ab
SherxonBoboyorov <sherxonboboyorov@gmail.com>
2023-03-30 10:42:51 +0500

    first commit

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\Admin\HomeController;
+use UniSharp\Laravel\LaravelFilemanager\Lfm;
+
+
+
+
+
+Auth::routes();
+
+Route::get('logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware(['role:admin'])->prefix('dashboard')->group(static function () {
+    Route::get('/', [HomeController::class, 'index'])->name('homeAdmin');
+    Route::resources([
+        //
+    ]);
+    Route::get('delete/{id}', [HouseController::class, 'delete'])->name('dashboard.delete');
+});
+
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => [ 'localeSessionRedirect', 'localizationRedirect', 'localeViewPath' ]
+    ], function(){
+         /** ADD ALL LOCALIZED ROUTES INSIDE THIS GROUP **/
+ });
+
+
+
+Route::group(['prefix' => 'laravel-filemanager', 'middleware' => ['web', 'auth']], function () {
+    UniSharp\LaravelFilemanager\Lfm::routes();
+});
+
+
